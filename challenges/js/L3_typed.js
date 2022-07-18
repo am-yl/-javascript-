da5714168efa7a3cd810d8b8b5af47fa89e4fd9a
am-yl <amyl@caramail.com>
2022-07-18 14:45:24 +0200

    L3 fini

--- challenges/js/L3_typed.js
+++ challenges/js/L3_typed.js
@@ -1,1 +1,9 @@
 console.log("Fichier L3_typed.js importé ✅");
+
+let type = new Typed('#typed', {
+    strings: ["potiteFrips jui tout pitite et plus tard je veux être la copine de potiCap", 'Frips', 'fatiguée', 'amoureuse de Cap! ♥'],
+    typeSpeed: 25,
+    backSpeed: 0,
+    fadeOut: true,
+    loop: true
+});
