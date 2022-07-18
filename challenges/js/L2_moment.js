3ee9f6c46df8da99bd16842bd779020e639c2cef
am-yl <amyl@caramail.com>
2022-07-18 12:20:46 +0200

    L2 progress (temps actuel)

--- challenges/js/L2_moment.js
+++ challenges/js/L2_moment.js
@@ -1,1 +1,6 @@
 console.log("Fichier L2_moment.js importé ✅");
+
+document.getElementById("format").addEventListener("click", function () {
+
+    document.getElementById("res_reel").textContent = moment().format('LLL:s');
+});
