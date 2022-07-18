a5d4e7d4edec32eedc09624cbafa63a26bfc7e95
am-yl <amyl@caramail.com>
2022-07-18 11:59:09 +0200

    edit tableaux p4

--- challenges/js/P4_section-dynamique.js
+++ challenges/js/P4_section-dynamique.js
@@ -9,13 +9,15 @@ let img = document.getElementById("img");
 let title = document.getElementById("title");
 let description = document.getElementById("description");
 
-let dog_tab = ["Ceci est un canidé mignon", "Les créatures canines ont 4 pattes et une jolie bouille, du moins pour la plupart.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/dog.jpg"];
-let business_tab = ["Mon associé et moi on fait masse de thune", "Parce qu'ici il n'y a que des winners, on génère masse pognon et on se met bien.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/business.jpg"];
-let motorcycle_tab = ["Une belle bécane comme on dit", "Ca coute moins cher en entretien, en carburant, en tout en fait. Par contre s'il pleut ou qu'il faut transporter des choses et/ou des gens t'es mal.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/motorcycle.jpg"];
-let beach_tab = ["Une belle plage pour oublier que le JS c'est facile mais c'est dur au début", "Un beau soleil, une bonne eau transparente, c'est à dire ni chaude ni froide, qui rafraichie sans avoir froid. Et du sable fin.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/beach.jpg"];
+let infos = [
+                ["Ceci est un canidé mignon", "Les créatures canines ont 4 pattes et une jolie bouille, du moins pour la plupart.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/dog.jpg"],
+                ["Mon associé et moi on fait masse de thune", "Parce qu'ici il n'y a que des winners, on génère masse pognon et on se met bien.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/business.jpg"],
+                ["Une belle bécane comme on dit", "Ca coute moins cher en entretien, en carburant, en tout en fait. Par contre s'il pleut ou qu'il faut transporter des choses et/ou des gens t'es mal.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/motorcycle.jpg"],
+                ["Une belle plage pour oublier que le JS c'est facile mais c'est dur au début", "Un beau soleil, une bonne eau transparente, c'est à dire ni chaude ni froide, qui rafraichie sans avoir froid. Et du sable fin.", "https://javascript.melvin-lemoine.me/assets/images/challenges/P4_section-dynamique/beach.jpg"]
+            ];
 
 dog.addEventListener("click", function() {
-    setMode(dog_tab);
+    setMode(infos[0]);
     dog.classList.add('is-success');
     business.classList.remove('is-success');
     motorcycle.classList.remove('is-success');
@@ -23,7 +25,7 @@ dog.addEventListener("click", function() {
 });
 
 business.addEventListener("click", function() {
-    setMode(business_tab);
+    setMode(infos[1]);
     dog.classList.remove('is-success');
     business.classList.add('is-success');
     motorcycle.classList.remove('is-success');
@@ -31,7 +33,7 @@ business.addEventListener("click", function() {
 });
 
 motorcycle.addEventListener("click", function() {
-    setMode(motorcycle_tab);
+    setMode(infos[2]);
     dog.classList.remove('is-success');
     business.classList.remove('is-success');
     motorcycle.classList.add('is-success');
@@ -39,7 +41,7 @@ motorcycle.addEventListener("click", function() {
 });
 
 beach.addEventListener("click", function() {
-    setMode(beach_tab);
+    setMode(infos[3]);
     dog.classList.remove('is-success');
     business.classList.remove('is-success');
     motorcycle.classList.remove('is-success');
