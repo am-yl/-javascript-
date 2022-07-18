a2fdfe9cf134744bd45a3a4289cff7136a91ad50
am-yl <amyl@caramail.com>
2022-07-18 17:05:34 +0200

    J3 Cookie Clicker

--- challenges/js/J3_cookie-clicker.js
+++ challenges/js/J3_cookie-clicker.js
@@ -1,1 +1,21 @@
 console.log("Fichier J3_cookie-clicker.js importé ✅");
+
+let countCookies = 0;
+let prices = ['50','150','500'];
+
+document.getElementById('cookie').addEventListener("click", function() {
+    addCookies(1);
+});
+
+document.getElementsByClassName('store__products_product').addEventListener("click", function() {
+
+});
+
+function addCookies(nb) {
+    countCookies += nb;
+    document.getElementById('nbCookie').textContent = Math.floor(countCookies);
+}
+
+function buyProduct(product) {
+
+}
