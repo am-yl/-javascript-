ca807decea65628b951448beff497105c54cd4c7
am-yl <amyl@caramail.com>
2022-07-18 15:01:42 +0200

    L4

--- challenges/js/L4_particles.js
+++ challenges/js/L4_particles.js
@@ -1,1 +1,5 @@
 console.log("Fichier L4_particles.js importé ✅");
+
+particlesJS.load('particles-js', '../assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
